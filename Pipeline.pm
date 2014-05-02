55655141a2d8146baa7763818dd40eba269b1786
Shawn Tan <shawn@wtf.sg>
2014-05-02 23:44:22 +0000

    Some documentation.

--- Pipeline.pm
+++ Pipeline.pm
@@ -285,35 +285,4 @@ sub reduce {
 	return $acc->();
 }
 
-
-if (0) {
-	my $sstart = Time::HiRes::gettimeofday();
-	reduce(
-		fileSource( $ARGV[0] ),
-		pipeline(
-			step { getWrapperData( $_ )->[0] },
-			filter { $_ },
-			filter { $_->{name} },
-			step { encode_json( $_ ) },
-		),
-		fileSink( $ARGV[1] )
-	);
-	my $send = Time::HiRes::gettimeofday();
-	print "Single Threaded time: ".($send - $sstart)."\n";
-
-	my $tstart = Time::HiRes::gettimeofday();
-	reduce(
-		fileSource( $ARGV[0] ),
-		multi( 4, pipeline(
-				step { getWrapperData( $_ )->[0] },
-				filter { $_ },
-				filter { $_->{name} },
-				step { encode_json( $_ ) },
-			) ),
-		fileSink( $ARGV[2] )
-	);
-	my $tend = Time::HiRes::gettimeofday();
-	print "Threaded time: ".($tend - $tstart)."\n";
-}
-
 1;
